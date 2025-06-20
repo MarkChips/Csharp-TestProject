3c6ef17d790f76f985d72712b65dd4ea801b8075
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-06-20 14:50:13 +0100

    Evaluate boolean expressions to make decisions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+﻿string permission = "bob";
+int level = 56;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+        Console.WriteLine("Welcome, Super Admin user.");
+    else
+        Console.WriteLine("Welcome, Admin user.");
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+        Console.WriteLine("Contact an Admin for access.");
+    else
+        Console.WriteLine("You do not have sufficient privileges.");
+}
+else
+    Console.WriteLine("You do not have sufficient privileges.");
