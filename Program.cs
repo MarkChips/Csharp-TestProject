672700f91546c0e80da705d60747cdeb23b965fb
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-09-16 17:30:47 +0100

    write code that validates string input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Code project 1 - write code that validates integer input
+﻿/* // Code project 1 - write code that validates integer input
 
 string? readResult;
 bool validEntry = false;
@@ -24,6 +24,35 @@ do
     {
         Console.WriteLine("Sorry, you entered an invalid number, please try again:");
     }
+} while (!validEntry);
+
+Console.WriteLine($"Your input value ({result}) has been accepted."); */
+
+// Code project 2 - write code that validates string input
+
+bool validEntry = false;
+string? result = null;
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User):");
+do
+{
+    string[] roles = { "administrator", "manager", "user" };
+    string? userInput = Console.ReadLine(), testCase;
+
+    if (userInput != null)
+    {
+        testCase = userInput.Trim().ToLower();
+        foreach (string role in roles)
+            if (testCase == role)
+            {
+                result = userInput.Trim();
+                validEntry = true;
+                break;
+            }
+        if (validEntry) continue;
+    }
+
+    Console.WriteLine($"The role name that you entered, \"{userInput}\" is not valid. Enter your role name (Administrator, Manager, or User):");
 } while (!validEntry);
 
 Console.WriteLine($"Your input value ({result}) has been accepted.");
